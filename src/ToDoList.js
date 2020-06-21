ba9d507eef01f2b15427043723d0ac3d380c91fc
Steven Anderson <saanderson1987@gmail.com>
2020-06-20 20:52:55 -0400

    add spacing to todo list

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectToDos } from "./redux/toDos";
-import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import List from "@material-ui/core/List";
 import AddToDoModal from "./AddToDoModal";
 import ToDoItem from "./ToDoItem";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: theme.spacing(4),
+  },
+}));
+
 const ToDoList = () => {
   const [isAddToDoModalOpen, setIsAddToDoModalOpen] = useState(false);
   const toDos = useSelector(selectToDos);
+  const classes = useStyles();
 
   return (
-    <Container>
-      <Button
-        onClick={() => setIsAddToDoModalOpen(true)}
-        startIcon={<AddCircleIcon />}
-      >
-        Add
-      </Button>
+    <Paper className={classes.paper} square>
+      <Box p={3}>
+        <Button
+          onClick={() => setIsAddToDoModalOpen(true)}
+          startIcon={<AddCircleIcon />}
+          variant="contained"
+          color="primary"
+        >
+          Add
+        </Button>
+      </Box>
       <AddToDoModal
         open={isAddToDoModalOpen}
         onClose={() => setIsAddToDoModalOpen(false)}
@@ -29,7 +42,7 @@ const ToDoList = () => {
           <ToDoItem item={item} index={index} key={index} />
         ))}
       </List>
-    </Container>
+    </Paper>
   );
 };
 
